b7f8cd8b8b2216a07e72bf932c3f9db9872d7423
trisolaris04 <trisolaris03@gmail.com>
2024-12-30 14:16:32 +0800

    提交assignment_2

--- assign2/main.cpp
+++ assign2/main.cpp
@@ -1,63 +1,98 @@
-/*
- * CS106L Assignment 2: Marriage Pact
- * Created by Haven Whitney with modifications by Fabio Ibanez & Jacob Roberts-Baca.
- *
- * Welcome to Assignment 2 of CS106L! Please complete each STUDENT TODO
- * in this file. You do not need to modify any other files.
- *
- */
+  /*
+  * CS106L Assignment 2: Marriage Pact
+  * Created by Haven Whitney with modifications by Fabio Ibanez & Jacob Roberts-Baca.
+  *
+  * Welcome to Assignment 2 of CS106L! Please complete each STUDENT TODO
+  * in this file. You do not need to modify any other files.
+  *
+  */
 
-#include <fstream>
-#include <iostream>
-#include <queue>
-#include <set>
-#include <string>
-#include <unordered_set>
+  #include <fstream>
+  #include <iostream>
+  #include <queue>
+  #include <set>
+  #include <string>
+  #include <unordered_set>
+  #include <ctime>
+  #include <sstream>
 
-#include "utils.h"
+  #include "utils.cpp"
 
-std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
+  std::string kYourName = "siamese forever"; // Don't forget to change this!
 
-/**
- * Takes in a file name and returns a set containing all of the applicant names as a set.
- *
- * @param filename  The name of the file to read.
- *                  Each line of the file will be a single applicant's name.
- * @returns         A set of all applicant names read from the file.
- *
- * @remark Feel free to change the return type of this function (and the function
- * below it) to use a `std::unordered_set` instead. If you do so, make sure
- * to also change the corresponding functions in `utils.h`.
- */
-std::set<std::string> get_applicants(std::string filename) {
-  // STUDENT TODO: Implement this function.
-}
+  /**
+   * Takes in a file name and returns a set containing all of the applicant names as a set.
+   *
+   * @param filename  The name of the file to read.
+   *                  Each line of the file will be a single applicant's name.
+   * @returns         A set of all applicant names read from the file.
+   *
+   * @remark Feel free to change the return type of this function (and the function
+   * below it) to use a `std::unordered_set` instead. If you do so, make sure
+   * to also change the corresponding functions in `utils.h`.
+   */
+  std::set<std::string> get_applicants(std::string filename) {
+    // STUDENT TODO: Implement this function.
+    std::set<std::string> applicants;
+    std::ifstream input(filename);
+    if (!input.is_open()) {
+      throw std::runtime_error("Could not open file.");
+    }
 
-/**
- * Takes in a set of student names by reference and returns a queue of names
- * that match the given student name.
- *
- * @param name      The returned queue of names should have the same initials as this name.
- * @param students  The set of student names.
- * @return          A queue containing pointers to each matching name.
- */
-std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
-  // STUDENT TODO: Implement this function.
-}
+    std::string name;
+    while (std::getline(input, name)) {
+      if (!name.empty()) { 
+      applicants.insert(name);
+      }
+    }
 
-/**
- * Takes in a queue of pointers to possible matches and determines the one true match!
- *
- * You can implement this function however you'd like, but try to do something a bit
- * more complicated than a simple `pop()`.
- *
- * @param matches The queue of possible matches.
- * @return        Your magical one true love.
- *                Will return "NO MATCHES FOUND." if `matches` is empty.
- */
-std::string get_match(std::queue<const std::string*>& matches) {
-  // STUDENT TODO: Implement this function.
-}
+    input.close();
 
-/* #### Please don't modify this call to the autograder! #### */
-int main() { return run_autograder(); }
+    return applicants;
+  }
+
+  /**
+   * Takes in a set of student names by reference and returns a queue of names
+   * that match the given student name.
+   *
+   * @param name      The returned queue of names should have the same initials as this name.
+   * @param students  The set of student names.
+   * @return          A queue containing pointers to each matching name.
+   */
+  std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
+    // STUDENT TODO: Implement this function.
+    std::istringstream iss(name);
+    std::string first_name, last_name;
+    iss >> first_name >> last_name;
+    std::queue<const std::string*> matches; 
+    for (const std::string& student : students) {
+      std::istringstream iss(student);
+      std::string student_first_name, student_last_name;
+      iss >> student_first_name >> student_last_name;
+      if (first_name[0] == student_first_name[0] && last_name[0] == student_last_name[0]) {
+        matches.push(&student);
+      }
+    }
+    return matches;
+  }
+
+  /**
+   * Takes in a queue of pointers to possible matches and determines the one true match!
+   *
+   * You can implement this function however you'd like, but try to do something a bit
+   * more complicated than a simple `pop()`.
+   *
+   * @param matches The queue of possible matches.
+   * @return        Your magical one true love.
+   *                Will return "NO MATCHES FOUND." if `matches` is empty.
+   */
+  std::string get_match(std::queue<const std::string*>& matches) {
+    // STUDENT TODO: Implement this function.
+    if (matches.empty()) {
+      return "NO MATCHES FOUND.";
+    }
+    return *matches.front();
+  }
+
+  /* #### Please don't modify this call to the autograder! #### */
+  int main() { return run_autograder(); }
